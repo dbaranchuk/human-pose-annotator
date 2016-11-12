bfea2b20566f05b77551913a3a6d1e22a2a31400
Dmitry Baranchuk <dmitrybaranchuk@gmail.com>
2016-11-13 02:04:43 +0400

    Add files via upload

---
+++ ImageNamesToJSON.py
@@ -0,0 +1,9 @@
+from os import listdir
+import json
+
+images = [f for f in listdir('./data') if (f.endswith('.jpg') or f.endswith('.png') or
+                                         f.endswith('.bmp') or f.endswith('.gif') or f.endswith('.jpeg'))]
+
+with open('data.json', 'w') as fp:
+    json.dump(images, fp)
+
