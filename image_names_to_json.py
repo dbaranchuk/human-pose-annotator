7cf2d6deea951d89120d5c5ae9b29fbab8f2aef1
Dmitry Baranchuk <dmitrybaranchuk@gmail.com>
2017-02-10 19:03:08 +0300

    Update and rename ImageNamesToJSON.py to image_names_to_json.py

--- image_names_to_json.py
+++ image_names_to_json.py
@@ -2,7 +2,7 @@ from os import listdir
 import json
 
 images = [f for f in listdir('./data') if (f.endswith('.jpg') or f.endswith('.png') or
-                                         f.endswith('.bmp') or f.endswith('.gif') or f.endswith('.jpeg'))]
+                                           f.endswith('.bmp') or f.endswith('.jpeg'))]
 
 with open('data.json', 'w') as fp:
     json.dump(images, fp)
